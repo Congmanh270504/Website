ff19a705f1dcd71682f45a2fb9fc8ecfb3b5a9bc
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2023-12-10 15:14:06 +0700

    first commit

---
+++ MyProject/script.js
@@ -0,0 +1,15 @@
+var colore = ["red", "organe", "yellow", "black"];
+
+
+// // color.unshift("white");// Thêm vào đầu array
+// // color.shift();// Xóa đầu array
+// // color.push(brown);// Thêm vào cuối array
+// // color.pop();// Xóa cuối array
+// index = color.indexOf("red"); // Tìm vị trí của giá trị cần tìm
+// console.log(index);
+// (...) Toán tử trải rộng dùng làm mở rộng các thành phần, phần tử trong mảng
+myColor.sort();// Sắp xếp theo chữ cái
+myColor.sort().reverse();// Sắp xếp ngược chữ cái 
+for (var color of myColor)// trg đó với color là biến giống biến i-> Biến chạy 
+    console.log(color);
+
